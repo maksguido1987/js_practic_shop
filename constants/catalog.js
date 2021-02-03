65e7ecc46caa3ad87899617bbe061d0126b580aa
Maksim <maksguido@gmail.com>
2021-02-03 17:23:44 +0300

    first commit

---
+++ constants/catalog.js
@@ -0,0 +1,26 @@
+const CATALOG = [
+   {
+      id: 1,
+      name: '',
+      image: "https://i.ibb.co/nk1tvP4/samsung-galaxy-A51-64-Gb.jpg",
+      price: '',
+   },
+   {
+      id: 1,
+      name: '',
+      image: "https://i.ibb.co/0QwhVQP/samsung-galaxy-a12-32-Gb-black.jpg",
+      price: '',
+   },
+   {
+      id: 1,
+      name: '',
+      image: "https://i.ibb.co/x3QkyRv/samsung-galaxy-A31-64-Gb.jpg",
+      price: '',
+   },
+   {
+      id: 1,
+      name: '',
+      image: "https://i.ibb.co/8mb9bJm/xiaomi-redmi-9-C-NFC-64-Gb.png",
+      price: '',
+   },
+]
