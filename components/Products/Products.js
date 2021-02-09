dd148ee160b0049f92958e5aaf16c4b5c1f58d2e
maksguido1987 <maksguido@gmail.com>
2021-02-09 13:22:40 +0300

    ...

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,7 +1,24 @@
 class Products {
+   constructor() {
+      this.classNameActive = " products-element__btn_active";
+      this.labelAdd = "Добавить в корзину";
+      this.labelRemove = "Удалить из корзины";
+   }
    render() {
+      const productsStore = localeStorageUtil.getProducts(); // данные из хранилища
       let htmlCatalog = '';
-      CATALOG.forEach(({id, name, image, price}) => {
+      CATALOG.forEach(({ id, name, image, price }) => {
+
+         let activeText = '';
+         let activeClass = '';
+
+         if (productsStore.indexOf(id) === -1) {  // если в ls нет id, значит добавляем, иначе удаляем
+            activeText = this.labelAdd;
+         } else {
+            activeText = "Удалить из корзины";
+            activeClass = this.classNameActive;
+         }
+
          htmlCatalog += `
          <li class="products-element">
             <span class="products-element__name">${name}</span>
@@ -9,7 +26,7 @@ class Products {
             <span class="products-element__price">
                👍 ${price.toLocaleString()} USD
             </span>
-            <button class="products-element__btn">Добавить в корзину</button>
+            <button class="products-element__btn${activeClass}">${activeText}</button>
          </li>
          `;
       });
